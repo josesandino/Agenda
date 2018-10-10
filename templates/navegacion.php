a31a4c3e504b61276dd99fca16b251cec5ba2aae
Jose Sandino <josesandinonio@gmail.com>
2018-10-09 21:14:37 -0600

    Catorceavo Commit
    Enlace de los archivos eventos e invitados

--- templates/navegacion.php
+++ templates/navegacion.php
@@ -45,8 +45,8 @@
             <span>Eventos</span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i> Ver Todos</a></li>
-            <li><a href="#"><i class="fa fa-plus-circle"></i> Agregar</a></li>
+            <li><a href="lista-eventos.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Ver Todos</a></li>
+            <li><a href="editar-eventos.php"><i class="fa fa-plus-circle"></i> Agregar</a></li>
           </ul>
         </li>
 
@@ -67,8 +67,8 @@
             <span>Invitados</span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i> Ver Todos</a></li>
-            <li><a href="#"><i class="fa fa-plus-circle"></i> Agregar</a></li>
+            <li><a href="lista-invitados.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Ver Todos</a></li>
+            <li><a href="crear-invitado.php"><i class="fa fa-plus-circle"></i> Agregar</a></li>
           </ul>
         </li>
 
